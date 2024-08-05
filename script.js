9629c8ac701ef013f186e280a331f50cc42eb9fb
G. D. McBain <gdmcbain@pm.me>
2024-08-05 16:40:11 +1000

    click to drop datum then tooltip r from there

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const image = document.querySelector(".image");
 const tooltip = document.querySelector(".tooltip");
+const datum = document.querySelector(".datum");
 
 image.addEventListener("mousemove", (e) => {
+  const dX = e.offsetX - datum.X;
+  const dY = e.offsetY - datum.Y;
+  const r = Math.hypot(dX, dY);
   tooltip.style.left = e.clientX + "px";
   tooltip.style.top = e.clientY - tooltip.offsetHeight - 10 + "px";
-  tooltip.textContent = `X: ${e.offsetX}, Y: ${e.offsetY}`;
+  tooltip.textContent = `r: ${r}`;
 });
 
 image.addEventListener("mouseover", () => {
@@ -13,4 +17,12 @@ image.addEventListener("mouseover", () => {
 
 image.addEventListener("mouseout", () => {
   tooltip.style.display = "none";
+});
+
+image.addEventListener("mousedown", (e) => {
+  datum.style.left = e.clientX + "px";
+  datum.style.top = e.clientY - datum.offsetHeight + "px";
+  datum.X = e.offsetX;
+  datum.Y = e.offsetY;
+  datum.textContent = "O";
 });
